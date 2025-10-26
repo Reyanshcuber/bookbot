619faa33eb9dbb080182c110d17896729af5dd41
Reyanshcuber <reyanshcubersat23@gmail.com>
2025-10-26 14:09:54 +0530

    added utf-8 support and improved readability

--- stats.py
+++ stats.py
@@ -1,27 +1,21 @@
 def get_no_of_words(filepath):
-    with open(filepath) as file:
-        contents=file.read()
-    words=contents.split()
-    no_of_words=len(words)
-    return no_of_words
-def get_character_frequency(filepath):
-    with open (filepath) as file:
-        contents=file.read()
-        final_contents=contents.lower()
-        dict={}
-        
-        for character in final_contents:
-            if character.isalpha()==False:
-                continue
-            if character in dict:
-                dict[character]+=1
-            else:
-                dict[character]=1
-        return dict
-def sort_on(dict):
-    return dict[1]
-def create_sorted_list(dict):
-    sorted_list=sorted(dict.items(), key=sort_on, reverse=True)
-    return sorted_list
+    with open(filepath, encoding="utf-8") as file:
+        contents = file.read()
+    words = contents.split()
+    return len(words)
 
-        
+def get_character_frequency(filepath):
+    with open(filepath, encoding="utf-8") as file:
+        contents = file.read().lower()
+    char_freq = {}
+    for character in contents:
+        if not character.isalpha():
+            continue
+        char_freq[character] = char_freq.get(character, 0) + 1
+    return char_freq
+
+def sort_on(item):
+    return item[1]
+
+def create_sorted_list(char_freq):
+    return sorted(char_freq.items(), key=sort_on, reverse=True)
